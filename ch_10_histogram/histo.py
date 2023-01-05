d55792364c8e6fb8737c0d3b2834baf75794b1d5
bagineer <bih198721@gmail.com>
2023-01-06 01:06:23 +0900

    Feature: Add equalizing histogram.

--- ch_10_histogram/histo.py
+++ ch_10_histogram/histo.py
@@ -47,9 +47,9 @@ img_norm = img_norm.astype(np.uint8)
 
 img_norm2 = cv2.normalize(img_f, None, 0, 255, cv2.NORM_MINMAX)
 
-hist = cv2.calcHist([img_dr], [0], None, [256], [0, 255])
-hist_norm = cv2.calcHist([img_norm], [0], None, [256], [0, 255])
-hist_norm2 = cv2.calcHist([img_norm2], [0], None, [256], [0, 255])
+hist = cv2.calcHist([img_dr], [0], None, [256], [0, 256])
+hist_norm = cv2.calcHist([img_norm], [0], None, [256], [0, 256])
+hist_norm2 = cv2.calcHist([img_norm2], [0], None, [256], [0, 256])
 
 imgs = {'original': img_dr, 'img_norm': img_norm, 'img_norm2': img_norm2}
 hists = {'original': hist, 'hist_norm': hist_norm, 'hist_norm2': hist_norm2}
@@ -67,6 +67,36 @@ for i, (k, v) in enumerate(hists.items()):
     plt.plot(v)
     plt.title(k)
 
+## Equalize
+h, w = img_dr.shape
+
+hist = cv2.calcHist([img_dr], [0], None, [256], [0, 256])
+cdf = hist.cumsum()
+cdf_m = np.ma.masked_equal(cdf, 0)
+cdf_m = (cdf_m - cdf_m.min()) / (h * w) * 255
+cdf = np.ma.filled(cdf_m, 0).astype(np.uint8)
+img_eq1 = cdf[img_dr]
+
+img_eq2 = cv2.equalizeHist(img_dr)
+
+hist_eq1 = cv2.calcHist([img_eq1], [0], None, [256], [0, 256])
+hist_eq2 = cv2.calcHist([img_eq2], [0], None, [256], [0, 256])
+
+imgs = {'original': img_dr, 'eq1': img_eq1, 'eq2': img_eq2}
+hists = {'original': hist, 'manual': hist_eq1, 'cv2.equlizeHist': hist_eq2}
+
+plt.figure('Equalize')
+for i, (k, v) in enumerate(imgs.items()):
+    plt.subplot(2, 3, i+1)
+    plt.imshow(v, 'gray', vmin=0, vmax=255)
+    plt.title(k)
+    plt.xticks([])
+    plt.yticks([])
+
+for i, (k, v) in enumerate(hists.items()):
+    plt.subplot(2, 3, i+4)
+    plt.plot(v)
+    plt.title(k)
 
 plt.show()
 
