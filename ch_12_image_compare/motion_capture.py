dbca3a5246e67de812b2f46f628511f047462683
bagineer <bih198721@gmail.com>
2023-01-08 18:37:34 +0900

    Feat: Add motion capture to motion_capture.py

---
+++ ch_12_image_compare/motion_capture.py
@@ -0,0 +1,50 @@
+import cv2
+import numpy as np
+
+THRESHOLD = 25
+MAX_DIFF = 5
+
+a, b, c = None, None, None
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+
+if cap.isOpened():
+    ret, a = cap.read()
+    ret, b = cap.read()
+
+    while ret:
+        ret, c = cap.read()
+        draw = c.copy()
+
+        if not ret:
+            break
+
+        a_gray = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
+        b_gray = cv2.cvtColor(b, cv2.COLOR_BGR2GRAY)
+        c_gray = cv2.cvtColor(c, cv2.COLOR_BGR2GRAY)
+
+        diff1 = cv2.absdiff(a_gray, b_gray)
+        diff2 = cv2.absdiff(b_gray, c_gray)
+
+        ret, diff1_t = cv2.threshold(diff1, THRESHOLD, 255, cv2.THRESH_BINARY)
+        ret, diff2_t = cv2.threshold(diff2, THRESHOLD, 255, cv2.THRESH_BINARY)
+        
+        diff = cv2.bitwise_and(diff1_t, diff2_t)
+
+        k = cv2.getStructuringElement(cv2.MORPH_CROSS, (3, 3))
+        diff = cv2.morphologyEx(diff, cv2.MORPH_OPEN, k)
+        diff_cnt = cv2.countNonZero(diff)
+
+        if diff_cnt > MAX_DIFF:
+            y, x = np.nonzero(diff)
+            cv2.rectangle(draw, (min(x), min(y)), (max(x), max(y)), (0, 255, 0), 2)
+        
+        stacked = np.vstack((draw, cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)))
+        cv2.imshow('motion sensor', stacked)
+
+        a, b = b, c
+        if cv2.waitKey(1) & 0xFF == 27:
+            break
+    
+cv2.destroyAllWindows()
