3dc0519446d2a4f1dca65eb4fb2ba62bb6ca792e
bagineer <bih198721@gmail.com>
2023-01-08 22:55:12 +0900

    Feat: Add resizing to resize.py

---
+++ ch_13_image_transform/resize.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('./cmes.png')
+h, w, _ = img.shape
+SCALE = 0.5
+
+dst1 = cv2.resize(img, (int(w*SCALE), int(h*SCALE)),
+                    interpolation=cv2.INTER_AREA)
+dst2 = cv2.resize(img, None, None, SCALE, SCALE, cv2.INTER_CUBIC)
+
+cv2.imshow('Original', img)
+cv2.imshow('dsize', dst1)
+cv2.imshow('fxfy', dst2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
