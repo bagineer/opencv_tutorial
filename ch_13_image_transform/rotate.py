df42c344f391fc379230b3384af8fd9c8f882ac6
bagineer <bih198721@gmail.com>
2023-01-08 23:16:02 +0900

    Feat: Add rotation with opencv to rotate.py

--- ch_13_image_transform/rotate.py
+++ ch_13_image_transform/rotate.py
@@ -17,9 +17,17 @@ m60 = np.float32([[np.cos(d60), -1*np.sin(d60), w//1.5],
 r30 = cv2.warpAffine(img, m30, (h, w))
 r60 = cv2.warpAffine(img, m60, (w, h))
 
+# cv2.getRotationMatrix2D
+m30_2 = cv2.getRotationMatrix2D((w//2, h//2), 30, 1)
+m60_2 = cv2.getRotationMatrix2D((w//2, h//2), 60, 1)
+
+img30 = cv2.warpAffine(img, m30_2, (w, h))
+img60 = cv2.warpAffine(img, m60_2, (w, h))
 
 cv2.imshow('Original', img)
 cv2.imshow('30', r30)
 cv2.imshow('60', r60)
+cv2.imshow('30 OpenCV', img30)
+cv2.imshow('60 OpenCV', img60)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
