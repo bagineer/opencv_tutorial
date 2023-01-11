8a147aa7bdf63100819e735169b07ea7415ea66f
bagineer <bih198721@gmail.com>
2023-01-12 01:06:10 +0900

    Refactor: Rename coppiedN to copiedN

--- ch_22/find_contour.py
+++ ch_22/find_contour.py
@@ -4,7 +4,7 @@ import numpy as np
 # img = cv2.imread('./sample.png')
 # h, w, _ = img.shape
 # img = cv2.resize(img, (w//2, h//2), interpolation=cv2.INTER_LINEAR)
-# coppied1 = img.copy()
+# copied1 = img.copy()
 
 # img_gray = img[:, :, 0]
 # ret, img_binary = cv2.threshold(img_gray, 200, 255, cv2.THRESH_BINARY_INV)
@@ -24,9 +24,9 @@ cv2.circle(img, (500, 100), 70, (255, 255, 255), -1)
 
 img_gray = img[:, :, 0]
 _, img_binary = cv2.threshold(img_gray, 127, 255, cv2.THRESH_BINARY_INV)
-coppied1 = img.copy()
-coppied2 = img.copy()
-coppied3 = img.copy()
+copied1 = img.copy()
+copied2 = img.copy()
+copied3 = img.copy()
 
 # 1. Contour External and Approximate None
 _, contour, hierarchy = cv2.findContours(img_binary, cv2.RETR_EXTERNAL,
@@ -41,11 +41,11 @@ cv2.putText(img, f'CHAIN_APPROX_NONE, Number of contours : {len(contour)}',
 # 2. Contour External and Approximate Simple
 _, contour2, hierarchy = cv2.findContours(img_binary, cv2.RETR_EXTERNAL,
                                                cv2.CHAIN_APPROX_SIMPLE)
-cv2.drawContours(coppied1, contour2, -1, (255, 0, 0), 8)
+cv2.drawContours(copied1, contour2, -1, (255, 0, 0), 8)
 for c in contour2:
     for p in c:
-        cv2.circle(coppied1, tuple(p[0]), 2, (0, 0, 255), -1)
-cv2.putText(coppied1, f'CHAIN_APPROX_SIMPLE, Number of contours : {len(contour2)}',
+        cv2.circle(copied1, tuple(p[0]), 2, (0, 0, 255), -1)
+cv2.putText(copied1, f'CHAIN_APPROX_SIMPLE, Number of contours : {len(contour2)}',
             (0, 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
 # 3. Contour Tree and Approximate Simple
@@ -55,14 +55,14 @@ print(hierarchy)
 
 for i, c in enumerate(contour3):
     color = np.random.randint(0, 255, 3).tolist()
-    cv2.drawContours(coppied2, contour3, i, color, 8)
-    cv2.putText(coppied2, str(i), tuple(c[0][0]), cv2.FONT_HERSHEY_PLAIN,
+    cv2.drawContours(copied2, contour3, i, color, 8)
+    cv2.putText(copied2, str(i), tuple(c[0][0]), cv2.FONT_HERSHEY_PLAIN,
                 1.5, (0, 0, 255), 2)
-cv2.putText(coppied2, f'TREE HiERARCHY & CHAIN_APPROX_SIMPLE, Number of contours : {len(contour2)}',
+cv2.putText(copied2, f'TREE HiERARCHY & CHAIN_APPROX_SIMPLE, Number of contours : {len(contour2)}',
             (0, 10), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 1)
 
 
-merged = np.vstack((img, coppied1, coppied2))
+merged = np.vstack((img, copied1, copied2))
 
 cv2.imshow('Contour', merged)
 cv2.waitKey(0)
