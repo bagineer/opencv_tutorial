10eb97cecacd64b8d76202e54e1bb783a2689a5f
bagineer <bih198721@gmail.com>
2023-01-12 14:47:23 +0900

    Feat: Add template matching

---
+++ ch_25/template_matching.py
@@ -0,0 +1,31 @@
+import cv2
+
+matching = cv2.imread('./figures.jpg')
+template = cv2.imread('./taekwonv3.jpg')
+th, tw, _ = template.shape
+win_name = 'Template Matching'
+
+cv2.imshow(win_name, template)
+
+methods = ['cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF_NORMED']
+
+for i, method_name in enumerate(methods):
+    img_draw = matching.copy()
+    method = eval(method_name)
+    res = cv2.matchTemplate(matching, template, method)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    print(method, min_val, max_val, min_loc, max_loc)
+
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        top_left, match_val = min_loc, min_val
+    else:
+        top_left, match_val = max_loc, max_val
+    bottom_right = (top_left[0] + tw, top_left[1] + th)
+    cv2.rectangle(img_draw, top_left, bottom_right, (0, 255, 0), 2)
+    cv2.putText(img_draw, str(match_val), top_left,
+                cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 1, cv2.LINE_AA)
+
+    cv2.imshow(method_name, img_draw)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
