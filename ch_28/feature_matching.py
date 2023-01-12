42cc0ca40137dde1ebd7e1f52946817a10b05d34
bagineer <bih198721@gmail.com>
2023-01-12 22:43:57 +0900

    Feat: Add BFMatcher + ORB

--- ch_28/feature_matching.py
+++ ch_28/feature_matching.py
@@ -37,5 +37,16 @@ res = cv2.drawMatches(img1, kp1, img2, kp2, matches, None,
                       flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
 cv2.imshow('BFMatcher + SURF', res)
 
+# BFMatcher + ORB
+detector = cv2.ORB_create()
+kp1, desc1 = detector.detectAndCompute(gray1, None)
+kp2, desc2 = detector.detectAndCompute(gray2, None)
+
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck = True)
+matches = matcher.match(desc1, desc2)
+res = cv2.drawMatches(img1, kp1, img2, kp2, matches, None,
+                      flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+cv2.imshow('BFMatcher + ORB', res)
+
 cv2.waitKey()
 cv2.destroyAllWindows()
