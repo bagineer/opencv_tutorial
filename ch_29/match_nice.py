a4bbe8f082b5bbacfe6017914ee500e27520d09c
bagineer <bih198721@gmail.com>
2023-01-13 00:28:35 +0900

    Feat: Add RANSAC

--- ch_29/match_nice.py
+++ ch_29/match_nice.py
@@ -10,6 +10,9 @@ img2 = cv2.imread(osp.join(file_path, 'sample2.jpg'))
 # img2 = cv2.resize(img2, (0, 0), None, 0.5, 0.5)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
+cv2.imshow('Template', img1)
+cv2.imshow('Target', img2)
+
 ## ORB
 # matcher.match
 orb = cv2.ORB_create()
@@ -48,18 +51,41 @@ cv2.imshow('ORB + KNN', res)
 src_pts = np.float32([kp_orb_1[m.queryIdx].pt for m in nice_matches])
 dst_pts = np.float32([kp_orb_2[m.trainIdx].pt for m in nice_matches])
 
-mat, mask = cv2.findHomography(src_pts, dst_pts)
+mat, _mask = cv2.findHomography(src_pts, dst_pts)
 h, w, _ = img1.shape
-
 pts = np.float32([[[0, 0]], [[0, h-1]], [[w-1, h-1]], [[w-1, 0]]])
 dst = cv2.perspectiveTransform(pts, mat)
 
-img_draw = cv2.polylines(img2, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
-res = cv2.drawMatches(img1, kp_orb_1, img2, kp_orb_2, nice_matches, None,
+img_draw = img2.copy()
+img_draw = cv2.polylines(img_draw, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+res = cv2.drawMatches(img1, kp_orb_1, img_draw, kp_orb_2, nice_matches, None,
                       flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
 cv2.imshow('ORB + findHomography', res)
 
-cv2.imshow('Template', img1)
-cv2.imshow('Target', img2)
+# RANSAC
+matches = matcher1.match(desc_orb_1, desc_orb_2)
+res1 = cv2.drawMatches(img1, kp_orb_1, img2, kp_orb_2, matches, None,
+                       flags = cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+src_pts = np.float32([kp_orb_1[m.queryIdx].pt for m in matches])
+dst_pts = np.float32([kp_orb_2[m.trainIdx].pt for m in matches])
+
+mat, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+h, w, _ = img1.shape
+pts = np.float32([[[0, 0]], [[0, h-1]], [[w-1, h-1]], [[w-1, 0]]])
+dst = cv2.perspectiveTransform(pts, mat)
+
+img_draw = img2.copy()
+img_draw = cv2.polylines(img_draw, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+matchesMask = mask.ravel().tolist()
+res2 = cv2.drawMatches(img1, kp_orb_1, img_draw, kp_orb_2, matches, None,
+                       matchesMask = matchesMask,
+                       flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+
+accuracy = float(mask.sum()) / mask.size
+print(f'Accuracy : {mask.sum()} / {mask.size} ({accuracy:.2f}%)')
+cv2.imshow('RANSAC ALL', res1)
+cv2.imshow('RANSAC Inlier', res2)
+
 cv2.waitKey()
 cv2.destroyAllWindows()
