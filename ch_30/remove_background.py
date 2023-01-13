0b2f350e67c68d5a4685cf889a2671f006d58b1f
bagineer <bih198721@gmail.com>
2023-01-13 14:59:26 +0900

    Feat: Add MOG2 with shades

--- ch_30/remove_background.py
+++ ch_30/remove_background.py
@@ -2,7 +2,6 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-win_name = 'Remove Background'
 
 if cap.isOpened():
     h = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
@@ -11,15 +10,22 @@ if cap.isOpened():
     fps = 30
     delay = int(1000/fps)
 
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+# MOG
+fgbg1 = cv2.bgsegm.createBackgroundSubtractorMOG()
+fgbg2 = cv2.createBackgroundSubtractorMOG2()
 while cap.isOpened():
     ret, frame = cap.read()
     if cv2.waitKey(delay) & 0xFF == 27 or not ret:
         break
     
-    fgmask = fgbg.apply(frame)
-    fgmask = cv2.cvtColor(fgmask, cv2.COLOR_GRAY2BGR)
-    cv2.imshow(win_name, np.hstack((frame, fgmask)))
+    fgmask1 = fgbg1.apply(frame)
+    fgmask1 = cv2.cvtColor(fgmask1, cv2.COLOR_GRAY2BGR)
+    fgmask2 = fgbg1.apply(frame)
+    fgmask2 = cv2.cvtColor(fgmask2, cv2.COLOR_GRAY2BGR)
+
+    cv2.imshow('Webcam', frame)
+    cv2.imshow('MOG1', fgmask1)
+    cv2.imshow('MOG2 with shades', fgmask2)
 
 cap.release()
 cv2.destroyAllWindows()
