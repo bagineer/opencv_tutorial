27691096ab65fc728eb2a402615e2f04e4180d70
bagineer <bih198721@gmail.com>
2023-01-16 09:36:06 +0900

    Feat: Modify reading and writing image file in save_image.py

--- ch_3_image_IO/save_image.py
+++ ch_3_image_IO/save_image.py
@@ -1,11 +1,13 @@
-from re import L
 import cv2
+import os.path as osp
 
-img_file = './asdf.png'
+file_path = osp.dirname(osp.abspath(__file__))
+file_name = 'asdf.png'
+img_file = osp.join(file_path, file_name)
 gray_img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
 
 if gray_img is not None:
-    cv2.imwrite('./gray_scale.png', gray_img)
+    cv2.imwrite(osp.join(file_path, 'gray_scale.png'), gray_img)
     cv2.destroyAllWindows()
 else:
     print('no image files.')
