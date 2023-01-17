f9889a080d7f05a4eace5ef2e1bd3bfd6094938e
bagineer <bih198721@gmail.com>
2023-01-17 09:43:42 +0900

    Feat: Modify reading and writing image file in roi.py

--- ch_6_roi/roi.py
+++ ch_6_roi/roi.py
@@ -1,32 +1,34 @@
-import cv2 as cv
+import cv2
 import os.path as osp
 
+file_path = osp.dirname(osp.abspath(__file__))
 file_name = 'sample.jpg'
-image = cv.imread(file_name)
+img_file = osp.join(file_path, file_name)
+image = cv2.imread(img_file)
 title = 'ROI'
 HEIGHT, WIDTH, _ = image.shape
 SCALE = 0.2
 
-cv.namedWindow(title)
+cv2.namedWindow(title)
 print(WIDTH, HEIGHT)
 
 
 if image is not None:
-    resized = cv.resize(image, (int(WIDTH*SCALE), int(HEIGHT*SCALE)))
+    resized = cv2.resize(image, (int(WIDTH*SCALE), int(HEIGHT*SCALE)))
     height, width, _ = resized.shape
     # print(width, height)
 
     # roi
     x, y, w, h = 425, 0, 300, 650
     roi = resized[y:y+h, x:x+w].copy()
-    cv.rectangle(resized, (x, y), (x+w, y+h), (0, 0, 255), 2)
+    cv2.rectangle(resized, (x, y), (x+w, y+h), (0, 0, 255), 2)
     resized[height-h:, width-w:] = roi
 
-    cv.imshow('roi', roi)
-    cv.imwrite('./roi.jpg', roi)
+    cv2.imshow('roi', roi)
+    cv2.imwrite(osp.join(file_path, 'roi.jpg'), roi)
 
-    cv.imshow(title, resized)
-    cv.waitKey(0)
-    cv.destroyAllWindows()
+    cv2.imshow(title, resized)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 else:
     exit(-1)
