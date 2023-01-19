cb89a3af00afa22c0512784b824297339d04bdcb
bagineer <bih198721@gmail.com>
2023-01-19 10:56:34 +0900

    Refactor: Modify reading images in add_rgba.py

--- ch_9_operation/add_rgba.py
+++ ch_9_operation/add_rgba.py
@@ -1,9 +1,14 @@
 import cv2
+import os.path as osp
 
-img_fg = cv2.imread('./sample.png', cv2.IMREAD_UNCHANGED)
+file_path = osp.dirname(osp.abspath(__file__))
+file_name = 'sample.png'
+img_file = osp.join(file_path, file_name)
+img_fg = cv2.imread(img_file, cv2.IMREAD_UNCHANGED)
 h, w, _ = img_fg.shape
+img_file_2 = osp.join(file_path, 'lion.jpg')
 img_fg = cv2.resize(img_fg, (w // 2, h // 2))
-img_bg = cv2.imread('./lion.jpg')
+img_bg = cv2.imread(img_file_2)
 
 _, mask = cv2.threshold(img_fg[:, :, 3], 1, 255, cv2.THRESH_BINARY)
 mask_inv = cv2.bitwise_not(mask)
