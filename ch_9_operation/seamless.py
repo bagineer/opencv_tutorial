90a1bc0b42b0fdf20265ae588688486123f93131
bagineer <bih198721@gmail.com>
2023-01-19 11:08:12 +0900

    Refactor: Modify reading image files in seamless.py

--- ch_9_operation/seamless.py
+++ ch_9_operation/seamless.py
@@ -1,20 +1,24 @@
 import cv2
 import numpy as np
+import os.path as osp
 from matplotlib import pyplot as plt
 
-src = cv2.imread('./chromakey.jpg')
-dts = cv2.imread('./antarctica.jpg')
+file_path = osp.dirname(osp.abspath(__file__))
+img_src = osp.join(file_path, 'chromakey.jpg')
+img_dst = osp.join(file_path, 'antarctica.jpg')
+src = cv2.imread(img_src)
+dst = cv2.imread(img_dst)
 
 mask = np.full_like(src, 255)
 
-h, w, _ = dts.shape
+h, w, _ = dst.shape
 center_point = (w//2, h//2)
-print(h, w, center_point, dts.shape)
+print(h, w, center_point, dst.shape)
 
-normal = cv2.seamlessClone(src, dts, mask, center_point, cv2.NORMAL_CLONE)
-mixed = cv2.seamlessClone(src, dts, mask, center_point, cv2.MIXED_CLONE)
+normal = cv2.seamlessClone(src, dst, mask, center_point, cv2.NORMAL_CLONE)
+mixed = cv2.seamlessClone(src, dst, mask, center_point, cv2.MIXED_CLONE)
 
-imgs = {'src': src, 'dts': dts, 'normal': normal, 'mixed':mixed}
+imgs = {'src': src, 'dst': dst, 'normal': normal, 'mixed':mixed}
 
 for i, (k, v) in enumerate(imgs.items()):
     plt.subplot(2, 2, i+1)
