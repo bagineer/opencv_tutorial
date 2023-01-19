c2a0cb1d456ba1ef2573331c09ad637d24a174d8
bagineer <bih198721@gmail.com>
2023-01-19 11:04:24 +0900

    Refactor: Modify reading image in mask.py

--- ch_9_operation/mask.py
+++ ch_9_operation/mask.py
@@ -1,8 +1,12 @@
 import cv2
 import numpy as np
+import os.path as osp
 from matplotlib import pyplot as plt
 
-img = cv2.imread('./tiger.jpg')
+file_path = osp.dirname(osp.abspath(__file__))
+file_name = 'tiger.jpg'
+img_file = osp.join(file_path, file_name)
+img = cv2.imread(img_file)
 
 mask = np.zeros_like(img)
 masking_value = 127
