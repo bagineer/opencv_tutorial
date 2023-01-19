8d1891dbc2ce521df73308298faa1a5256f4dbb2
bagineer <bih198721@gmail.com>
2023-01-19 11:11:35 +0900

    Refactor: Modify reading image files in track_bar.py

--- ch_9_operation/track_bar.py
+++ ch_9_operation/track_bar.py
@@ -1,4 +1,5 @@
 import cv2
+import os.path as osp
 
 win_name = 'Alpha Blending'
 tb_name = 'Fade'
@@ -8,15 +9,18 @@ def onChange(x):
     blended = cv2.addWeighted(img1, alpha, img2, 1-alpha, 0)
     cv2.imshow(win_name, blended)
 
-img1 = cv2.imread('./lion.jpg')
-img2 = cv2.imread('./tiger.jpg')
+file_path = osp.dirname(osp.abspath(__file__))
+img_file1 = osp.join(file_path, 'lion.jpg')
+img_file2 = osp.join(file_path, 'tiger.jpg')
+img1 = cv2.imread(img_file1)
+img2 = cv2.imread(img_file2)
 h1, w1, _ = img1.shape
 h2, w2, _ = img2.shape
 mh, mw = min(h1, h2), min(w1, w2)
 dh, dw = h1 - mh, w2 - mw
 img1 = img1[dh//2:dh//2+mh, dw//2:dw//2+mw, :]  # crop bigger image by small image sizes
 
-cv2.imshow(win_name, img1)
+cv2.imshow(win_name, img2)
 cv2.createTrackbar(tb_name, win_name, 0, 100, onChange)
 
 cv2.waitKey(0)
